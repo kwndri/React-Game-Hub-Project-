caa12aa903121dd20d0559b6c1c555cb84e9342c
kwndri <kondritsas14@gmail.com>
2025-09-04 13:27:01 +0300

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "581a97cf08af4414a5902eb281944096",
+    key: "" //Add your key,
   },
 });
